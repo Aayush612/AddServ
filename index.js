e8f6e03aef58d22066748e6881bd787c33ed7141
Aayush612 <aayush612@gmail.com>
2020-06-08 03:36:03 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
   var map = L.map('map').setView([23.35, 85.08], 2);
-
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+//https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png
+  L.tileLayer('',
     {
       attribution: 'Tiles by <a href="http://mapc.org">MAPC</a>, Data by <a href="http://mass.gov/mgis">MassGIS</a>',
       maxZoom: 12,
@@ -35,13 +35,30 @@
 					var colp=feature.properties.value1*4;
 					return({
 						weight: 2, 
-						color: "red", 
-						opacity: 0.2,
+						color: "lightred", 
+						opacity: 0.8,
 						fillColor: "white", 
 						fillOpacity:  0.7
 					})
 						
-					}
+					},
+		onEachFeature: function (feature, layer) {
+        layer.on('click', function (e) {
+            this.setStyle(highlight);
+            e.target.feature.properties.highlight = true;
+            // hideLayers(feature,layer);
+			console.log(layer)
+        });
+        map.on('click', function (e) {
+            //having trouble with resetStyle, so just change it back
+            layer.setStyle({
+             'color': 'blue',
+             'weight': 1,
+             'opacity': 0.8
+           });
+          // showLayers();
+        });
+    }
 		}
 		)
 	}
@@ -52,7 +69,7 @@
 					var colp=feature.properties.value1*4;
 					return({
 						weight: 1, 
-						color: "grey", 
+						color: "darkgrey", 
 						opacity: 0.5,
 						fillColor: "beige", 
 						fillOpacity:  0.7
@@ -72,14 +89,14 @@
 	function afterExec(){
 		console.log("-----")
 		level_one.addTo(map)
-				
+		
 		
 
 	}
 	
 	map.on('zoomend', function(e) {
 		console.log(e)
-		if (map.getZoom() <4){
+		if (map.getZoom() <1){
 		map.removeLayer(level_one);//1st geoJSON layer
 		}else{
 		map.addLayer(level_one);
@@ -94,8 +111,8 @@
 		map.removeLayer(level_three); //3rd geoJSON layer
 		}else{
 		map.addLayer(level_three);
-		map.removeLayer(level_one);
-		map.removeLayer(level_two);
+		level_two.bringToFront()
+		
 
 		}
 		});
@@ -104,3 +121,27 @@
 		console.log(map.getBounds())
 	
 	})
+	
+	function hideLayers (){
+  this.layer.eachLayer(function(layer){
+    if(!layer.feature.properties.highlight){
+      map.removeLayer(layer);
+    }
+  });
+}
+
+function showLayers (){
+  this.layer.eachLayer(function(layer){
+    layer.feature.properties.highlight = false;
+    map.addLayer(layer);
+  });
+}
+
+ var highlight = {
+       'weight': 2,
+       'opacity': 0.8,
+	   'fillColor':'lightgreen',
+	   'fillOpacity':0.4
+   };
+   
+  
